00c93382d414aadeaaa07c5a8b83ad8c3d063712
Laura Manzur <lmanzurv@gmail.com>
2014-12-31 12:55:03 -0500

    Added elimination of Hop-By-Hop headers Included Server header to the response

--- proxy_server/__init__.py
+++ proxy_server/__init__.py
@@ -1,3 +1,14 @@
+HOP_BY_HOP = [
+    'connection',
+    'keep-alive',
+    'aroxy-authenticate',
+    'roxy-authorization',
+    'te',
+    'trailers',
+    'transfer-encoding',
+    'upgrade'
+]
+
 RESTFUL_HEADER = {
     'Content-type': 'application/json',
     'Accept': 'application/json',
@@ -7,11 +18,16 @@ RESTFUL_HEADER = {
 HTTP_API_KEY = 'HTTP_API_KEY'
 HTTP_USER_TOKEN = 'HTTP_USER_TOKEN'
 HTTP_FROM = 'HTTP_X_FORWARDED_FOR'
+HTTP_SERVER = 'Server'
 
 # Header names from response
 USER_TOKEN = 'user-token'
 API_KEY = 'api-key'
 CLIENT_IP = 'client-ip'
+
+# Response headers
+HEADER_SERVER = 'Server'
+VALUE_SERVER = 'Django Proxy Server'
 
 # Response content
 MESSAGE = 'message'
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-proxy-server',
-    version='0.3.2',
+    version='0.4',
     packages=find_packages(),
 
     # Dependencies
